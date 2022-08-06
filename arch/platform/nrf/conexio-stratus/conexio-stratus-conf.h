b4de29345684d7e2aebb9f93943b0081d709ea86
rajeev1986 <rajeev.piyare@hotmail.com>
2022-08-05 22:07:19 -0500

    add conexio stratus platform

---
+++ arch/platform/nrf/conexio-stratus/conexio-stratus-conf.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2021 Rajeev Piyare <rajeev@conexiotech.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*---------------------------------------------------------------------------*/
+/**
+ * \addtogroup nrf-platforms
+ * @{
+ *
+ * \addtogroup Conexio Stratus[nrf9160]
+ * @{
+ *
+ * \file
+ *         Conexio Stratus DK specific configuration.
+ * \author
+ *         Rajeev Piyare <rajeev@conexiotech.com>
+ */
+/*---------------------------------------------------------------------------*/
+#ifndef CONEXIO_STRATUS_PDK_CONF_H
+#define CONEXIO_STRATUS_PDK_CONF_H
+/*---------------------------------------------------------------------------*/
+#define PLATFORM_HAS_UARTE              1
+#define PLATFORM_HAS_BUTTON             1
+#define PLATFORM_SUPPORTS_BUTTON_HAL    1
+/*---------------------------------------------------------------------------*/
+#define NRF_BUTTON1_PIN     12
+#define NRF_BUTTON1_PORT    0
+/*---------------------------------------------------------------------------*/
+/* BLUE LED on the main DK */
+#define NRF_LED1_PIN        3
+#define NRF_LED1_PORT       0
+
+/* Orange LED on the Stratus shield */
+#define NRF_LED2_PIN        28
+#define NRF_LED2_PORT       0
+/*---------------------------------------------------------------------------*/
+#define LEDS_CONF_COUNT     2
+/*---------------------------------------------------------------------------*/
+#define NRF_UARTE0_TX_PIN   6
+#define NRF_UARTE0_TX_PORT  0
+#define NRF_UARTE0_RX_PIN   5
+#define NRF_UARTE0_RX_PORT  0
+/*---------------------------------------------------------------------------*/
+#endif /* CONEXIO_STRATUS_PDK_CONF_H */
+/*---------------------------------------------------------------------------*/
+/** 
+ * @} 
+ * @} 
+ */
